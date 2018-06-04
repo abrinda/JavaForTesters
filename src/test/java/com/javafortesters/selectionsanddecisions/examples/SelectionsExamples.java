e74d76cca5d5f45e798c75927ed85b93ad9a4ee9
Elena.Brinda <elena.brinda@imobiliare.ro>
2018-06-04 15:20:58 +0300

    test

--- src/test/java/com/javafortesters/selectionsanddecisions/examples/SelectionsExamples.java
+++ src/test/java/com/javafortesters/selectionsanddecisions/examples/SelectionsExamples.java
@@ -82,4 +82,180 @@ public class SelectionsExamples {
         assertTrue(url.startsWith("http://"));
         assertEquals("http://www.seleniumsimplified.com", url);
     }
+
+    @Test
+    public void ifElseOnThruthy(){
+        boolean thruthy = true;
+        if (thruthy)
+            assertTrue(thruthy);
+        else
+            assertFalse(thruthy);
+    }
+
+    @Test
+    public void ifElseOnThruthyBraces(){
+        boolean thruthy=true;
+        if (thruthy){
+            assertTrue(thruthy);
+            assertFalse(!thruthy);
+        }else{
+            assertFalse(thruthy);
+        }
+    }
+
+    @Test
+    public void ifElseNestedAddHttp(){
+        String url = "seleniumsimplified.com";
+        if (url.startsWith("http")){
+            //do nothing, the url is fine
+        }else{
+            if (!url.startsWith("www")){
+                url="www." + url;
+            }
+            url = addHttp(url);
+        }
+        assertTrue(url.startsWith("http://"));
+        assertEquals("http://www.seleniumsimplified.com", url);
+    }
+
+    @Test
+    public void nestedIfElseHorror(){
+        boolean truthy=true;
+        boolean falsey=false;
+        if (truthy){
+            if(!falsey){
+                if(truthy && !falsey){
+                    if (falsey || truthy){
+                        assertTrue(truthy);
+                        assertFalse(falsey);
+                    }
+                }
+            }else {
+                assertTrue(truthy);
+                assertFalse(falsey);
+            }
+        }else{
+            if(!truthy){
+                if (falsey){
+                    assertTrue(falsey);
+                    assertFalse(truthy);
+                }else{
+                    assertTrue(truthy);
+                    assertFalse(falsey);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void switchExample(){
+        assertEquals("M", likelyGenderIs("sir"));
+        assertEquals("M", likelyGenderIs("mr"));
+        assertEquals("M", likelyGenderIs("master"));
+        assertEquals("F", likelyGenderIs("miss"));
+        assertEquals("F", likelyGenderIs("mrs"));
+        assertEquals("F", likelyGenderIs("ms"));
+        assertEquals("F", likelyGenderIs("lady"));
+        assertEquals("F", likelyGenderIs("madam"));
+    }
+
+    public String likelyGenderIs(String title){
+        String likelyGender;
+
+        switch(title.toLowerCase()){
+            case "sir":
+                likelyGender = "M";
+                break;
+            case "mr":
+                likelyGender = "M";
+                break;
+            case "master":
+                likelyGender = "M";
+                break;
+            default:
+                likelyGender = "F";
+                break;
+        }
+        return likelyGender;
+    }
+
+    @Test
+    public void switchOnShortCode(){
+        assertEquals("United Kingdom", shortCodeIs("UK"));
+        assertEquals("United States", shortCodeIs("US"));
+        assertEquals("United States", shortCodeIs("USA"));
+        assertEquals("France", shortCodeIs("FR"));
+        assertEquals("Sweden", shortCodeIs("SE"));
+        assertEquals("Rest Of World", shortCodeIs("RO"));
+        assertEquals("Rest Of World", shortCodeIs("MD"));
+        assertEquals("Rest Of World", shortCodeIs("CZ"));
+        assertEquals("Rest Of World", shortCodeIs("ES"));
+        assertEquals("Rest Of World", shortCodeIs("POR"));
+    }
+
+    public String shortCodeIs(String shortCode){
+        String country;
+
+        switch(shortCode.toUpperCase()){
+            case "UK":
+                country = "United Kingdom";
+                break;
+            case "US":
+            case "USA":
+                country = "United States";
+                break;
+            case "FR":
+                country = "France";
+                break;
+            case "SE":
+                country = "Sweden";
+                break;
+            default:
+                country = "Rest Of World";
+                break;
+        }
+
+        return country;
+    }
+
+    @Test
+    public void switchOnInt(){
+        assertEquals("One", numberIs(1));
+        assertEquals("Two", numberIs(2));
+        assertEquals("Three", numberIs(3));
+        assertEquals("Four", numberIs(4));
+        assertEquals("Too big", numberIs(9));
+        assertEquals("Too big", numberIs(Integer.MAX_VALUE));
+        assertEquals("Too small", numberIs(0));
+        assertEquals("Too small", numberIs(Integer.MIN_VALUE));
+    }
+
+    public String numberIs(int number){
+        String howIsTheNumber="";
+
+        switch(number){
+            case 1:
+                howIsTheNumber="One";
+                break;
+            case 2:
+                howIsTheNumber="Two";
+                break;
+            case 3:
+                howIsTheNumber="Three";
+                break;
+            case 4:
+                howIsTheNumber="Four";
+                break;
+            default:
+                if (number<1){
+                    howIsTheNumber= "Too small";
+                }
+                if (number>4){
+                    howIsTheNumber= "Too big";
+                }
+                break;
+        }
+
+        return howIsTheNumber;
+    }
 }
