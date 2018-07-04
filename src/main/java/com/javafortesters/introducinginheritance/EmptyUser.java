dd219747e6e86d0aa2299f783ad4816bde797a1a
abrinda <brindaadina@gmail.com>
2018-07-04 14:17:18 +0300

    chap 11

---
+++ src/main/java/com/javafortesters/introducinginheritance/EmptyUser.java
@@ -0,0 +1,4 @@
+package com.javafortesters.introducinginheritance;
+
+public class EmptyUser {
+}
---
+++ src/test/java/com/javafortesters/introducinginheritance/examples/InheritanceExamples.java
@@ -0,0 +1,4 @@
+package com.javafortesters.introducinginheritance.examples;
+
+public class IntroducingInheritance {
+}
